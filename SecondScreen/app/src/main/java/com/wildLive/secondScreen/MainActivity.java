2af30a9fcd63b5c825b24224417d4bf1b7793b5c
ariTheBrain <ariane.demleitner@stud.uni-regensburg.de>
2018-08-11 22:24:34 +0200

    #16 #38 #41 Added custom cast button dialogs (main activity and dialog handler) for initializing device connection via signalR

--- SecondScreen/app/src/main/java/com/wildLive/secondScreen/MainActivity.java
+++ SecondScreen/app/src/main/java/com/wildLive/secondScreen/MainActivity.java
@@ -6,16 +6,54 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogHandler.OnInputListener {
 
-    public  final String TAG = "MEK_Plugin_Activity";   // tags for logging
-    private Button buttonMainNext;                      // button for switching to next activity (temporary!)
-    private Button connectionButton;                    // button for connecting devices via signalR (temporary)!)
-    private SignalRClient SRClient = null;              // instance of signalR client behaviour
+    // src:
+    // ************
+    // https://developer.android.com/guide/topics/ui/dialogs
+
+    // interfaces from dialogHandler
+    @Override
+    public void sendInput(String input, int buttonId) {
+        switchCastButton(buttonId);
+        // registering signalR client
+        SRClient = new SignalRClient(messageCallback);
+    }
+    @Override
+    public void sendInput(int buttonId) {
+        switchCastButton(buttonId);
+    }
+
+    // switching between cast buttons in options menu
+    private void switchCastButton(int buttonId) {
+        switch (buttonId) {
+            case 0:
+                break;
+            case 1:
+                if (activeCast.isVisible()) {
+                    activeCast.setVisible(false);
+                    inactiveCast.setVisible(true);
+                } else {
+                    inactiveCast.setVisible(false);
+                    activeCast.setVisible(true);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public  final String TAG = "MEK_Plugin_Activity";       // tags for logging
+    private Button buttonMainNext;                          // button for switching to next activity (temporary!)
+    private SignalRClient SRClient = null;                  // instance of signalR client behaviour
+    private MenuItem activeCast;
+    private MenuItem inactiveCast;
+    private DialogHandler dialog = new DialogHandler();     // for handling dialogs (options menu)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +61,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // registering signalR client
-        SRClient = new SignalRClient(messageCallback);
-
         // registering button listener
         addListenerOnButtons();
     }
 
+    @Override
     public boolean onCreateOptionsMenu (Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // registering menu (cast) items for further on-click handling (e.g. visibility)
+        activeCast = menu.findItem(R.id.action_cast_main);
+        inactiveCast = menu.findItem(R.id.action_cast_connected_main);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_cast_main) {
+            // calling popup dialog (for deactivating connection)
+            callCastActivation();
+            return true;
+        } else if (item.getItemId() == R.id.action_cast_connected_main) {
+            // calling popup dialog (for activating connection)
+            callCastDeactivation();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void callCastActivation() {
+        // setting new component values for dialog
+        dialog.setDialogTitle("Geräteverbindung");
+        dialog.setDialogDescription("Geben Sie hier die ID zur Verbindung mit dem First Screen ein:", true);
+        dialog.setDialogActionButtons("VERBINDEN", true, "ABBRECHEN", true);
+
+        // showing dialog
+        dialog.show(getSupportFragmentManager(), "connectionDialogHandler");
+    }
+
+    private void callCastDeactivation() {
+        // setting new component values for dialog
+        dialog.setDialogTitle("Geräteverbindung");
+        dialog.setDialogDescription("Möchten Sie die Geräteverbindung trennen?", false);
+        dialog.setDialogActionButtons("JA", true, "NEIN", true);
+
+        // showing dialog
+        dialog.show(getSupportFragmentManager(), "disconnectionDialogHandler");
     }
 
     public void addListenerOnButtons() {
@@ -47,17 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 // switching to next activity on button click
                 Intent intent = new Intent(context, VideoBibActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        // registering button for establishing connection between devices
-        connectionButton = (Button) findViewById(R.id.connectingButton);
-        connectionButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                // connecting via signalR-client
-                SRClient.sendMsg("connection request from android phone");
             }
         });
     }
